636cb2f74118494be0ffa4b6dba14bf62681921f
Afesoh Brain <brainafesoh@gmail.com>
2020-11-07 21:25:44 +0100

    🏷️ Implementing allLanguages query

--- api/main/language/graphql/language.queries.ts
+++ api/main/language/graphql/language.queries.ts
@@ -1,4 +1,6 @@
 import { log, schema } from "nexus"
+import { LanguageRepoPrisma } from "../language.repo"
+import { LanguageService } from "../language.service"
 
 schema.extendType({
   type: "Query",
@@ -8,6 +10,11 @@ schema.extendType({
       list: true,
       resolve: async (parent, args, ctx) => {
         try {
+          const languageService = new LanguageService(
+            new LanguageRepoPrisma({ instance: ctx.db }),
+          )
+          const allLanguages = languageService.allLanguages()
+          return allLanguages
         } catch (error) {
           log.error(error)
           return error
